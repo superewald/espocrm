99a39eff61a363128e8e3ded6714dc8ab2cc2fbc
SuperEwald <code@superewald.net>
2022-07-14 22:05:24 +0200

    add wkhtmltopdf engine classes

---
+++ application/Espo/Tools/Pdf/WkPdf/WkEntityProcessor.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Espo\Modules\Academy\Tools\Pdf\WkPdf;
+
+use Espo\Core\Utils\Config;
+use Espo\Core\Utils\Json;
+use Espo\Core\Htmlizer\TemplateRendererFactory;
+use Espo\Core\Htmlizer\TemplateRenderer;
+
+use Espo\ORM\Entity;
+
+use Espo\Tools\Pdf\Template;
+use Espo\Tools\Pdf\Data;
+use Espo\Tools\Pdf\Params;
+
+class WkEntityProcessor
+{
+    private Config $config;
+    private TemplateRendererFactory $templateRendererFactory;
+
+    public function __construct(Config $config, TemplateRendererFactory $templateRendererFactory)
+    {
+        $this->config = $config;
+        $this->templateRendererFactory = $templateRendererFactory;
+    }
+
+    public function process(WkPdf $pdf, Template $template, Entity $entity, Params $params, Data $data)
+    {
+        $renderer = $this->templateRendererFactory
+            ->create()
+            ->setApplyAcl($params->applyAcl())
+            ->setEntity($entity)
+            ->setData($data->getAdditionalTemplateData());
+        
+
+        $opts = [
+            'orientation' => $template->getPageOrientation(),
+            'margin-bottom' => $template->getBottomMargin(),
+            'margin-top' => $template->getTopMargin(),
+            'margin-left' => $template->getLeftMargin(),
+            'margin-right' => $template->getRightMargin(),
+            'title' => $template->getTitle(),
+        ];
+
+        if($template->getPageFormat() === 'Custom') {
+            $opts['page-width'] = $template->getPageWidth();
+            $opts['page-height'] = $template->getPageHeight();
+        } else {
+            $opts['page-size'] = $template->getPageFormat();
+        }
+
+        $pdf->setOptions($opts);
+
+        $header = tempnam(sys_get_temp_dir(), 'header-tpl');
+        $footer = tempnam(sys_get_temp_dir(), 'footer-tpl');
+
+        if($template->hasHeader()) {
+            file_put_contents($header.'.html', "<!DOCTYPE html><html>".$renderer->renderTemplate($template->getHeader())."</html>");
+            $pdf->setOptions([
+                'header-html' => $header.'.html'
+            ]);
+        }
+
+        if($template->hasFooter()) {
+            file_put_contents($footer.'.html', "<!DOCTYPE html><html>".$renderer->renderTemplate($template->getFooter())."</html>");
+            $pdf->setOptions([
+                'footer-html' => $footer.'.html'
+            ]);
+        }
+        
+        $htmlBody = $renderer->renderTemplate($template->getBody());
+        $pdf->addPage($htmlBody);
+
+        unlink($header);
+        unlink($footer);
+    }
+}
---
+++ application/Espo/Tools/Pdf/WkPdf/WkPdf.php
@@ -0,0 +1,9 @@
+<?php
+namespace Espo\Modules\Academy\Tools\Pdf\WkPdf;
+
+use mikehaertl\wkhtmlto\Pdf;
+
+class WkPdf extends Pdf
+{
+    public $binary = "/wkhtmltopdf";
+}
